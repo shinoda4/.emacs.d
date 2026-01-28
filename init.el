b903d4e91deec5b3792fb10170fb53ced3448c9c
shinoda4 <lindesong666@gmail.com>
2026-01-28 17:18:46 +0800

    refact the configuration structures.

--- init.el
+++ init.el
@@ -1,49 +1,10 @@
 
+
+(add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
+(require 'options)
+(require 'keymaps)
+
 ;; User defined keymaps should after C-x r <KEY>.
-
-(add-to-list 'exec-path "/usr/local/bin")
-(setenv "PATH" (concat "/usr/local/bin:" (getenv "PATH")))
-
-(add-to-list 'exec-path "/Users/carl/.local/bin")
-(setenv "PATH" (concat "/Users/carl/.local/bin:" (getenv "PATH")))
-
-(setq custom-file
-      (if (getenv "XDG_CONFIG_HOME")
-	  (expand-file-name "emacs/.emacs.custom.el" (getenv "XDG_CONFIG_HOME"))
-	(cond
-	 ((eq system-type 'darwin)
-	  (expand-file-name "~/.config/emacs/.emacs.custom.el")
-	  )
-	 (t (expand-file-name ".emacs.custom.el" user-emacs-directory))
-	 )))
-
-(load custom-file 'noerror)
-(recentf-mode 1)
-(tool-bar-mode -1)
-(menu-bar-mode -1)
-(scroll-bar-mode -1)
-(column-number-mode 1)
-(global-display-line-numbers-mode 1)
-(setq dired-kill-when-opening-new-dired-buffer t)
-(setq global-auto-revert-non-file-buffers t)
-(setq auto-revert-verbose nil)
-
-(setq confirm-kill-emacs 'yes-or-no-p)
-(setq-default case-fold-search t)
-
-(setq tab-width 4)
-(setq inhibit-startup-message t)
-
-;; fonts
-
-(defun get-default-font()
-  (cond
-   ((eq system-type 'darwin)
-    "Iosevka-20")
-   ))
-
-(add-to-list 'default-frame-alist
-	     `(font . ,(get-default-font)))
 
 ;; Define a keybinding for hello world.
 
@@ -53,15 +14,6 @@
   (insert "Hello World!"))
 
 (global-set-key (kbd "C-c h") 'greet)
-
-
-(setq treesit-language-source-alist
-      '((rust "https://github.com/tree-sitter/tree-sitter-rust.git" "v0.21.2")
-        (go "https://github.com/tree-sitter/tree-sitter-go.git" "v0.23.4")
-        (typescript "https://github.com/tree-sitter/tree-sitter-typescript.git" "v0.23.2" "typescript/src")
-        (tsx "https://github.com/tree-sitter/tree-sitter-typescript.git" "v0.23.2" "tsx/src")
-        (python "https://github.com/tree-sitter/tree-sitter-python.git" "v0.23.6" "src")))
-
 
 (defvar bootstrap-version)
 (let ((bootstrap-file
@@ -96,20 +48,20 @@
   :straight t
   :init
   (vertico-mode)
-  :bind (
-	 :map vertico-map
-         ("DEL" . vertico-directory-delete-char)))
+  :bind
+  (:map vertico-map
+	("DEL" . vertico-directory-delete-char)))
 
 ;; live preview
 (use-package consult
   :straight t
-  :bind (
-         ("C-x f" . consult-find)
-         ("C-x b" . consult-buffer)
-         ("C-s" . consult-line)
-         ("M-g i" . consult-imenu)
-         ("C-x r r" . consult-recent-file)
-         ("M-y" . consult-yank-pop)))
+  :bind
+  (("C-x f" . consult-find)
+    ("C-x b" . consult-buffer)
+    ("C-s" . consult-line)
+    ("M-g i" . consult-imenu)
+    ("C-x r r" . consult-recent-file)
+    ("M-y" . consult-yank-pop)))
 
 ;; using space as separator
 (use-package orderless
@@ -164,11 +116,8 @@
   ("C-=" . er/expand-region)
   ("C-+" . er/contract-region))
 
-(with-eval-after-loxXad 'eglot
+(with-eval-after-load 'eglot
   (add-to-list 'eglot-server-programs
                `(python-mode . ("ty" "server"))))
 
 (add-hook 'python-mode-hook 'eglot-ensure)
-
-
-
