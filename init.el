3c39269f429010834a234736dec389adf2ffd388
shinoda4 <lindesong666@gmail.com>
2026-02-22 11:33:09 +0800

    first commit.

---
+++ init.el
@@ -0,0 +1,134 @@
+(setq custom-file (expand-file-name ".emacs.custom.el" user-emacs-directory))
+
+(add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
+
+;; straight.el
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name
+        "straight/repos/straight.el/bootstrap.el"
+        (or (bound-and-true-p straight-base-dir)
+            user-emacs-directory)))
+      (bootstrap-version 7))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+(setq package-enable-at-startup nil)
+(straight-use-package 'use-package)
+
+;; editor configurations
+(recentf-mode 1)
+(tool-bar-mode 0)
+(menu-bar-mode 0)
+(scroll-bar-mode 0)
+(global-display-line-numbers-mode 1)
+
+(use-package ido-completing-read+
+  :straight t
+  :config
+  (require 'ido-completing-read+)
+  )
+
+;; (ido-mode 1)
+;; (ido-everywhere 1)
+;; (ido-ubiquitous-mode 1)
+
+(add-to-list 'default-frame-alist '(font . "IBM Plex Mono-20"))
+
+(use-package gruber-darker-theme
+  :straight t
+  :config
+  (load-theme 'gruber-darker t)
+  )
+
+(use-package magit
+  :straight t)
+
+;; (use-package smex
+;;   :straight t)
+
+(use-package orderless
+  :straight t
+  :custom
+  (completion-styles '(orderless basic))
+  (completion-category-overrides '((file (styles partial-completion))))
+  ;; (completion-pcm-leading-wildcard t)
+  )
+
+;; VERTical Interactive COmpletion
+(use-package vertico
+  :straight t
+  ;; :custom
+  ;; (vertico-scroll-margin 0) ;; Different scroll margin
+  ;; (vertico-count 20) ;; Show more candidates
+  ;; (vertico-resize t) ;; Grow and shrink the Vertico minibuffer
+  ;; (vertico-cycle t) ;; Enable cycling for `vertico-next/previous'
+  :init
+  (vertico-mode))
+
+(use-package consult
+  :straight t
+  :config
+  (setq consult-preview-key '(:debounce 0.5 any))
+  (global-set-key (kbd "C-c f") 'consult-fd)
+  (global-set-key (kbd "C-c r") 'consult-recent-file)
+  )
+
+(use-package paredit
+  :straight t
+  )
+
+(use-package marginalia
+  :straight t
+  :init
+  (marginalia-mode))
+
+;; for buffer completition
+(use-package corfu
+  :straight t
+  :custom
+  (corfu-auto t)
+  (corfu-auto-prefix 2)
+  (corfu-auto-delay 0.1)
+  (corfu-cycle t)
+  :init
+  (global-corfu-mode))
+
+(use-package cape
+  :straight t
+  :config
+  (add-to-list 'completion-at-point-functions #'cape-file))
+
+(use-package avy
+  :straight t
+  :config
+  (global-set-key (kbd "C-:") 'avy-goto-char)
+  (global-set-key (kbd "C-'") 'avy-goto-char-2)
+  (global-set-key (kbd "M-g f") 'avy-goto-line)
+  (global-set-key (kbd "M-g w") 'avy-goto-word-1)
+  (global-set-key (kbd "M-g e") 'avy-goto-word-0)
+)
+
+(use-package exec-path-from-shell
+  :straight t
+  :config
+  (when (memq window-system '(mac ns x))
+    (exec-path-from-shell-initialize)))
+
+(use-package savehist
+  :straight t
+  :init
+  (savehist-mode))
+
+(require 'eglot)
+
+(use-package rust-mode
+  :straight t)
+
+(load custom-file 'noerror)
